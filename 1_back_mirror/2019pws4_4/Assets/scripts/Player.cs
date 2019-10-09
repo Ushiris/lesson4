683e4e3107b799355e766d10f21bc0f9388d5fb4
uni_note_007 <ushiriscco@gmail.com>
2019-10-09 18:49:57 +0900

    create sea
    long long long long long long long long long long long long long long long long sea.

---
+++ 1_back_mirror/2019pws4_4/Assets/scripts/Player.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 3;
+    public float jumpForce = 3;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool[] input_key =
+            {
+            Input.GetKey(KeyCode.D),
+            Input.GetKey(KeyCode.A),
+            Input.GetKey(KeyCode.W),
+            Input.GetKey(KeyCode.S),
+            Input.GetKey(KeyCode.Space)
+        };
+
+        if(input_key[0]&&input_key[1])
+        {
+            input_key[0] = false;
+            input_key[1] = false;
+        }
+        if(input_key[2]&&input_key[3])
+        {
+            input_key[2] = false;
+            input_key[3] = false;
+        }
+
+        Vector3 move=Vector3.zero;
+        if(input_key[0])
+        {
+            transform.Rotate(Vector3.up, 0.3f);
+        }
+        else if(input_key[1])
+        {
+            transform.Rotate(Vector3.up, -0.3f);
+        }
+        
+        if(input_key[2])
+        {
+            move += transform.forward;
+        }
+        else if(input_key[3])
+        {
+            move -= transform.forward;
+        }
+
+        float y = rb.velocity.y;
+
+        rb.velocity = move * speed;
+        rb.velocity = new Vector3(rb.velocity.x, y, rb.velocity.z);
+
+        if(input_key[4])
+        {
+            rb.AddForce(0, jumpForce, 0);
+        }
+
+    }
+}
---
+++ 2_water/2019pws4-4-2/Assets/scripts/RaySarcher.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaySarcher : MonoBehaviour
+{
+
+    [SerializeField] CustomRenderTexture _texture;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Material m = _texture.material;
+        m.SetFloat("_BulletX", -1.0f);
+        m.SetFloat("_BulletZ", -1.0f);
+
+        if(Input.GetMouseButton(0))
+        {
+            Ray ray = new Ray();
+            RaycastHit hit = new RaycastHit();
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray.origin,ray.direction,out hit,Mathf.Infinity))
+            {
+                if(hit.collider.gameObject.CompareTag("Water"))
+                {
+                    Vector3 p = new Vector3(hit.point.x * 0.2f, 0.0f, hit.point.z * 0.2f);
+                    float u = p.x * 0.5f * 0.5f;
+                    float v = p.z * 0.5f * 0.5f;
+
+                    m.SetFloat("_BulletX", u);
+                    m.SetFloat("_BulletZ", v);
+                }
+            }
+        }
+    }
+}
